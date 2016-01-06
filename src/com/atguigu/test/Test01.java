d245b0a3b925c12d228ffa4059125a8db1498bf0
shangxiaobin <18092456631@163.com>
2016-01-06 22:28:02 +0800

    第一个类

---
+++ src/com/atguigu/test/Test01.java
@@ -0,0 +1,10 @@
+package com.atguigu.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是我的第一个git项目");
+	}
+
+}
